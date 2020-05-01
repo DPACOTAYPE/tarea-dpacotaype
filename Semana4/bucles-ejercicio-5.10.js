2f847110e78a8b695571d8e49ac79626289f601f
DPACOTAYPE <maritza_18_4@hotmail.com>
2020-05-01 18:27:57 -0500

    Mejorando el ejercicio 5.10

--- Semana4/bucles-ejercicio-5.10.js
+++ Semana4/bucles-ejercicio-5.10.js
@@ -6,15 +6,19 @@ let num1=[];
 let num2=[];
 let suma=[];
 // let valores1=0;
-for(let i=0;i<4;i++){
+for(let i=0;i<10;i++){
 let valores1=+prompt("Ingrese los 10 primeros números:");
 num1.push(valores1);
-let valores2=+prompt("Ingrese los 10 segundos números:");
-num2.push(valores2);
 }
 console.log(`Los 10 primeros números son: ${num1}`);
+
+for(let k=0;k<10;k++){
+    let valores2=+prompt("Ingrese los 10 segundos números:");
+    num2.push(valores2);
+    }
+    
 console.log(`Los 10 segundos números son: ${num2}`);
-for(j=0;j<4;j++){
-    suma[j]=valores1[j]+valores2[j];
+for(j=0;j<10;j++){
+    suma[j]=num1[j]+num2[j];
 }
 console.log(`La suma de los valores num1 más num2 es: ${suma}`);
